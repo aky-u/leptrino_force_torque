a183ffbad28d0155854339467d60ecc3accfc9a6
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-05-20 19:09:59 +0200

    Add .clang-format file and refactor LeptrinoForceTorqueSensor class for improved parameter handling and logging

--- include/leptrino/system.hpp
+++ include/leptrino/system.hpp
@@ -4,45 +4,72 @@
 #include <hardware_interface/sensor_interface.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <string>
-#include "leptrino/rs_comm.h"
+
 #include "leptrino/pComResInternal.h"
 #include "leptrino/pCommon.h"
+#include "leptrino/rs_comm.h"
 
 namespace leptrino_force_torque
 {
-  class LeptrinoForceTorqueSensor : public hardware_interface::SensorInterface
-  {
-  public:
-    ~LeptrinoForceTorqueSensor() override;
-    hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
-    hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
-    hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
-    hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
-    hardware_interface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &previous_state) override;
-    hardware_interface::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &previous_state) override;
-    std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-    hardware_interface::return_type read(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+class LeptrinoForceTorqueSensor : public hardware_interface::SensorInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(LeptrinoForceTorqueSensor)
 
-  private:
-    std::string g_com_port_;
-    int g_rate_;
-    int g_com_ok_;
-    UCHAR CommRcvBuff_[256];
-    UCHAR CommSendBuff_[1024];
-    UCHAR SendBuff_[512];
-    double conversion_factor_[FN_Num];
+  // ~LeptrinoForceTorqueSensor() override;
 
-    // functions
-    void App_Init();
-    void App_Close(rclcpp::Logger logger);
-    ULONG SendData(UCHAR *pucInput, USHORT usSize);
-    void GetProductInfo(rclcpp::Logger logger);
-    void GetLimit(rclcpp::Logger logger);
-    void SerialStart(rclcpp::Logger logger);
-    void SerialStop(rclcpp::Logger logger);
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
 
-    // state interfaces
-    std::vector<double> hw_sensor_states_;
-  };
+  hardware_interface::CallbackReturn
+  on_configure(const rclcpp_lifecycle::State &previous_state) override;
+
+  // hardware_interface::CallbackReturn
+  // on_cleanup(const rclcpp_lifecycle::State &previous_state) override;
+
+  hardware_interface::CallbackReturn
+  on_shutdown(const rclcpp_lifecycle::State &previous_state) override;
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  hardware_interface::return_type read(const rclcpp::Time &time,
+                                       const rclcpp::Duration &period) override;
+
+  /// Get the logger of the SensorInterface.
+  /**
+   * \return logger of the SensorInterface.
+   */
+  rclcpp::Logger get_logger() const { return *logger_; }
+
+  /// Get the clock of the SensorInterface.
+  /**
+   * \return clock of the SensorInterface.
+   */
+  rclcpp::Clock::SharedPtr get_clock() const { return clock_; }
+
+private:
+  std::string g_com_port_;
+  int g_rate_;
+  int g_com_ok_;
+  UCHAR CommRcvBuff_[256];
+  UCHAR CommSendBuff_[1024];
+  UCHAR SendBuff_[512];
+  double conversion_factor_[FN_Num];
+
+  // functions
+  void App_Init();
+  void App_Close(rclcpp::Logger logger);
+  ULONG SendData(UCHAR *pucInput, USHORT usSize);
+  void GetProductInfo(rclcpp::Logger logger);
+  void GetLimit(rclcpp::Logger logger);
+  void SerialStart(rclcpp::Logger logger);
+  void SerialStop(rclcpp::Logger logger);
+
+  // Objects for logging
+  std::shared_ptr<rclcpp::Logger> logger_;
+  rclcpp::Clock::SharedPtr clock_;
+
+  // Store the sensor states for the simulated robot
+  std::vector<double> hw_sensor_states_;
+};
 } // namespace leptrino_force_torque
 #endif // _LEPTRINO_SYSTEM_HPP
--- src/system.cpp
+++ src/system.cpp
@@ -2,172 +2,239 @@
 
 namespace leptrino_force_torque
 {
-  LeptrinoForceTorqueSensor::~LeptrinoForceTorqueSensor()
+// LeptrinoForceTorqueSensor::~LeptrinoForceTorqueSensor()
+// {
+//   // If the controller manager is shutdown via Ctrl + C
+//   on_cleanup(rclcpp_lifecycle::State());
+// }
+
+hardware_interface::CallbackReturn
+LeptrinoForceTorqueSensor::on_init(const hardware_interface::HardwareInfo &info)
+{
+  // Initialize the sensor interface
+  if (SensorInterface::on_init(info) != hardware_interface::CallbackReturn::SUCCESS)
   {
-    // If the controller manager is shutdown via Ctrl + C
-    on_cleanup(rclcpp_lifecycle::State());
+    return hardware_interface::CallbackReturn::ERROR;
   }
 
-  hardware_interface::CallbackReturn LeptrinoForceTorqueSensor::on_init(const hardware_interface::HardwareInfo &info)
+  // Read the parameters from the hardware interface
+  if (info_.hardware_parameters.find("com_port") != info_.hardware_parameters.end())
   {
-    // Initialize the sensor interface
-    if (SensorInterface::on_init(info) != hardware_interface::CallbackReturn::SUCCESS)
-    {
-      return hardware_interface::CallbackReturn::ERROR;
+    g_com_port_ = info_.hardware_parameters.at("com_port");
+  }
+  else
+  {
+    RCLCPP_WARN(rclcpp::get_logger("LeptrinoForceTorqueSensor"),
+                "Port is not defined, trying /dev/ttyUSB0");
+    g_com_port_ = "/dev/ttyUSB0";
+  }
+
+  if (info_.hardware_parameters.find("rate") != info_.hardware_parameters.end())
+  {
+    g_rate_ = std::stoi(info_.hardware_parameters.at("rate"));
+  }
+  else
+  {
+    RCLCPP_WARN(rclcpp::get_logger("LeptrinoForceTorqueSensor"),
+                "Rate is not defined, using maximum 1.2 kHz");
+    g_rate_ = 1200;
+  }
+
+  // Initialize the object for logging
+  logger_ = std::make_shared<rclcpp::Logger>(rclcpp::get_logger(
+      "controller_manager.resource_manager.hardware_component.sensor.ExternalRRBotFTSensor"));
+  clock_ = std::make_shared<rclcpp::Clock>(rclcpp::Clock());
+
+  // Initialize the state interfaces
+  hw_sensor_states_.resize(info_.sensors[0].state_interfaces.size(),
+                           std::numeric_limits<double>::quiet_NaN());
+
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+hardware_interface::CallbackReturn
+LeptrinoForceTorqueSensor::on_configure(const rclcpp_lifecycle::State &previous_state)
+{
+  // Initialize the application
+  App_Init();
+
+  // Get the product information
+  GetProductInfo(rclcpp::get_logger("LeptrinoForceTorqueSensor"));
+
+  // Get the limit information
+  GetLimit(rclcpp::get_logger("LeptrinoForceTorqueSensor"));
+
+  // Start the sensor
+  SerialStart(rclcpp::get_logger("LeptrinoForceTorqueSensor"));
+
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+// hardware_interface::CallbackReturn
+// LeptrinoForceTorqueSensor::on_cleanup(const rclcpp_lifecycle::State &previous_state)
+// {
+//   // Stop the sensor
+//   SerialStop(rclcpp::get_logger("LeptrinoForceTorqueSensor"));
+
+//   // Close the application
+//   App_Close(rclcpp::get_logger("LeptrinoForceTorqueSensor"));
+
+//   return hardware_interface::CallbackReturn::SUCCESS;
+// }
+
+hardware_interface::CallbackReturn
+LeptrinoForceTorqueSensor::on_shutdown(const rclcpp_lifecycle::State &previous_state)
+{
+  // Shutdown the application
+  App_Close(rclcpp::get_logger("LeptrinoForceTorqueSensor"));
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface> LeptrinoForceTorqueSensor::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+
+  // export sensor state interface
+  for (uint i = 0; i < info_.sensors[0].state_interfaces.size(); i++)
+  {
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+        info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_[i]));
+  }
+
+  return state_interfaces;
+}
+
+hardware_interface::return_type LeptrinoForceTorqueSensor::read(const rclcpp::Time &time,
+                                                                const rclcpp::Duration &period)
+{
+}
+
+// ----------------------------------------------------------------------------
+// Private functions
+// ----------------------------------------------------------------------------
+void LeptrinoForceTorqueSensor::App_Init()
+{
+  int rt;
+
+  // Initialize the Comm port
+  g_com_ok_ = NG;
+  rt = Comm_Open(g_com_port_.c_str());
+  if (rt == OK)
+  {
+    Comm_Setup(460800, PAR_NON, BIT_LEN_8, 0, 0, CHR_ETX);
+    g_com_ok_ = OK;
+  }
+}
+
+void LeptrinoForceTorqueSensor::App_Close(rclcpp::Logger logger)
+{
+  RCLCPP_DEBUG(logger, "Application close\n");
+
+  if (g_com_ok_ == OK)
+  {
+    Comm_Close();
+  }
+}
+
+ULONG LeptrinoForceTorqueSensor::SendData(UCHAR *pucInput, USHORT usSize)
+{
+  USHORT usCnt;
+  UCHAR ucWork;
+  UCHAR ucBCC = 0;
+  UCHAR *pucWrite = &CommSendBuff_[0];
+  USHORT usRealSize;
+
+  // Reformat the data
+  *pucWrite = CHR_DLE; // DLE
+  pucWrite++;
+  *pucWrite = CHR_STX; // STX
+  pucWrite++;
+  usRealSize = 2;
+
+  for (usCnt = 0; usCnt < usSize; usCnt++)
+  {
+    ucWork = pucInput[usCnt];
+    if (ucWork == CHR_DLE)
+    {                      // if data is 0x10 then add 0x10
+      *pucWrite = CHR_DLE; // DLE
+      pucWrite++;          // writing destination
+      usRealSize++;        // actual size
+      // Do not calculate BCC!
     }
-
-    // Read the parameters from the hardware interface
-    if (info_.hardware_parameters.find("com_port") != info_.hardware_parameters.end())
-    {
-      g_com_port_ = info_.hardware_parameters.at("com_port");
-    }
-    else
-    {
-      RCLCPP_WARN(rclcpp::get_logger("LeptrinoForceTorqueSensor"), "Port is not defined, trying /dev/ttyUSB0");
-      g_com_port_ = "/dev/ttyUSB0";
-    }
-
-    if (info_.hardware_parameters.find("rate") != info_.hardware_parameters.end())
-    {
-      g_rate_ = std::stoi(info_.hardware_parameters.at("rate"));
-    }
-    else
-    {
-      RCLCPP_WARN(rclcpp::get_logger("LeptrinoForceTorqueSensor"), "Rate is not defined, using maximum 1.2 kHz");
-      g_rate_ = 1200;
-    }
-
-    // Initialize the state interfaces
-
-    return hardware_interface::CallbackReturn::SUCCESS;
+    *pucWrite = ucWork; // data
+    ucBCC ^= ucWork;    // BCC
+    pucWrite++;         // writing destination
+    usRealSize++;       // actual size
   }
 
-  // ----------------------------------------------------------------------------
-  // Private functions
-  // ----------------------------------------------------------------------------
-  void LeptrinoForceTorqueSensor::App_Init()
-  {
-    int rt;
+  *pucWrite = CHR_DLE; // DLE
+  pucWrite++;
+  *pucWrite = CHR_ETX; // ETX
+  ucBCC ^= CHR_ETX;    // BCC
+  pucWrite++;
+  *pucWrite = ucBCC; // BCC
+  usRealSize += 3;
 
-    // Initialize the Comm port
-    g_com_ok_ = NG;
-    rt = Comm_Open(g_com_port_.c_str());
-    if (rt == OK)
-    {
-      Comm_Setup(460800, PAR_NON, BIT_LEN_8, 0, 0, CHR_ETX);
-      g_com_ok_ = OK;
-    }
-  }
+  Comm_SendData(&CommSendBuff_[0], usRealSize);
 
-  void LeptrinoForceTorqueSensor::App_Close(rclcpp::Logger logger)
-  {
-    RCLCPP_DEBUG(logger, "Application close\n");
+  return OK;
+}
 
-    if (g_com_ok_ == OK)
-    {
-      Comm_Close();
-    }
-  }
+void LeptrinoForceTorqueSensor::GetProductInfo(rclcpp::Logger logger)
+{
+  USHORT len;
 
-  ULONG LeptrinoForceTorqueSensor::SendData(UCHAR *pucInput, USHORT usSize)
-  {
-    USHORT usCnt;
-    UCHAR ucWork;
-    UCHAR ucBCC = 0;
-    UCHAR *pucWrite = &CommSendBuff_[0];
-    USHORT usRealSize;
+  RCLCPP_INFO(logger, "Get sensor information");
+  len = 0x04;                 // data length
+  SendBuff_[0] = len;         // length
+  SendBuff_[1] = 0xFF;        // sensor no.
+  SendBuff_[2] = CMD_GET_INF; // command type
+  SendBuff_[3] = 0;           // reserve
 
-    // Reformat the data
-    *pucWrite = CHR_DLE; // DLE
-    pucWrite++;
-    *pucWrite = CHR_STX; // STX
-    pucWrite++;
-    usRealSize = 2;
+  SendData(SendBuff_, len);
+}
 
-    for (usCnt = 0; usCnt < usSize; usCnt++)
-    {
-      ucWork = pucInput[usCnt];
-      if (ucWork == CHR_DLE)
-      {                      // if data is 0x10 then add 0x10
-        *pucWrite = CHR_DLE; // DLE
-        pucWrite++;          // writing destination
-        usRealSize++;        // actual size
-        // Do not calculate BCC!
-      }
-      *pucWrite = ucWork; // data
-      ucBCC ^= ucWork;    // BCC
-      pucWrite++;         // writing destination
-      usRealSize++;       // actual size
-    }
+void LeptrinoForceTorqueSensor::GetLimit(rclcpp::Logger logger)
+{
+  USHORT len;
 
-    *pucWrite = CHR_DLE; // DLE
-    pucWrite++;
-    *pucWrite = CHR_ETX; // ETX
-    ucBCC ^= CHR_ETX;    // BCC
-    pucWrite++;
-    *pucWrite = ucBCC; // BCC
-    usRealSize += 3;
+  RCLCPP_INFO(logger, "Get sensor limit");
+  len = 0x04;                   // data length
+  SendBuff_[0] = len;           // length
+  SendBuff_[1] = 0xFF;          // sensor no.
+  SendBuff_[2] = CMD_GET_LIMIT; // command type
+  SendBuff_[3] = 0;             // reserve
 
-    Comm_SendData(&CommSendBuff_[0], usRealSize);
+  SendData(SendBuff_, len);
+}
 
-    return OK;
-  }
+void LeptrinoForceTorqueSensor::SerialStart(rclcpp::Logger logger)
+{
+  USHORT len;
 
-  void LeptrinoForceTorqueSensor::GetProductInfo(rclcpp::Logger logger)
-  {
-    USHORT len;
+  RCLCPP_INFO(logger, "Start sensor");
+  len = 0x04;                    // data length
+  SendBuff_[0] = len;            // length
+  SendBuff_[1] = 0xFF;           // sensor no.
+  SendBuff_[2] = CMD_DATA_START; // command type
+  SendBuff_[3] = 0;              // reserve
 
-    RCLCPP_INFO(logger, "Get sensor information");
-    len = 0x04;                 // data length
-    SendBuff_[0] = len;         // length
-    SendBuff_[1] = 0xFF;        // sensor no.
-    SendBuff_[2] = CMD_GET_INF; // command type
-    SendBuff_[3] = 0;           // reserve
+  SendData(SendBuff_, len);
+}
 
-    SendData(SendBuff_, len);
-  }
+void LeptrinoForceTorqueSensor::SerialStop(rclcpp::Logger logger)
+{
+  USHORT len;
 
-  void LeptrinoForceTorqueSensor::GetLimit(rclcpp::Logger logger)
-  {
-    USHORT len;
+  RCLCPP_INFO(logger, "Stop sensor");
+  len = 0x04;                   // data length
+  SendBuff_[0] = len;           // length
+  SendBuff_[1] = 0xFF;          // sensor no.
+  SendBuff_[2] = CMD_DATA_STOP; // command type
+  SendBuff_[3] = 0;             // reserve
 
-    RCLCPP_INFO(logger, "Get sensor limit");
-    len = 0x04;                   // data length
-    SendBuff_[0] = len;           // length
-    SendBuff_[1] = 0xFF;          // sensor no.
-    SendBuff_[2] = CMD_GET_LIMIT; // command type
-    SendBuff_[3] = 0;             // reserve
-
-    SendData(SendBuff_, len);
-  }
-
-  void LeptrinoForceTorqueSensor::SerialStart(rclcpp::Logger logger)
-  {
-    USHORT len;
-
-    RCLCPP_INFO(logger, "Start sensor");
-    len = 0x04;                    // data length
-    SendBuff_[0] = len;            // length
-    SendBuff_[1] = 0xFF;           // sensor no.
-    SendBuff_[2] = CMD_DATA_START; // command type
-    SendBuff_[3] = 0;              // reserve
-
-    SendData(SendBuff_, len);
-  }
-
-  void LeptrinoForceTorqueSensor::SerialStop(rclcpp::Logger logger)
-  {
-    USHORT len;
-
-    RCLCPP_INFO(logger, "Stop sensor");
-    len = 0x04;                   // data length
-    SendBuff_[0] = len;           // length
-    SendBuff_[1] = 0xFF;          // sensor no.
-    SendBuff_[2] = CMD_DATA_STOP; // command type
-    SendBuff_[3] = 0;             // reserve
-
-    SendData(SendBuff_, len);
-  }
+  SendData(SendBuff_, len);
+}
 
 } // namespace leptrino_force_torque
 
@@ -175,4 +242,5 @@ namespace leptrino_force_torque
 // Export plugin
 // ----------------------------------------------------------------------------
 #include "pluginlib/class_list_macros.hpp"
-PLUGINLIB_EXPORT_CLASS(leptrino_force_torque::LeptrinoForceTorqueSensor, hardware_interface::SensorInterface)
+PLUGINLIB_EXPORT_CLASS(leptrino_force_torque::LeptrinoForceTorqueSensor,
+                       hardware_interface::SensorInterface)
